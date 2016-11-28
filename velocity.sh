0b1ad34e268707a1130b28bedf23066ad9b47718
Anandu <amdas222@gmail.com>
2016-11-28 17:30:41 +0530

    Updated help message and readme

--- velocity.sh
+++ velocity.sh
@@ -3,13 +3,17 @@
 #Printing Usage instruction string
 USAGE="Usage: $0 -u [url] -m [mode]
 
-  -u  --url 		Websocket url
-  -m  --mode 		listen/send
-  -t  --to              Username to which the messages have to be send
-  -d  --dump 		Filename to dump the messages with full path
-  -c  --chat        Chat message to be send to the user
-  -s  --sleep 		Time to sleep between each message
-  -h  --help 		Display this help and exit"
+  -u    --url 		Websocket url
+  -m    --mode 		listen/send/compare
+  -t    --ro            Username to which the messages have to be send
+  -d    --dump 		Filename to dump the messages with full path
+  -c    --chat          Chat message to be send to the user
+  -l    --length        Length of each random message to be generated
+  -n    --number        Number of random messages to generate.
+  -s    --sleep 	Time to sleep between each message
+  -f1   --file1         First file whose entries will be matched against file2
+  -f2   --file2         Second file whose entries will be matched against entries in file1
+  -h    --help 		Display this message and exit"
 
 if [[ $# -eq 0 || $1 == "-h" ]]; then
 	echo "$USAGE"
@@ -64,8 +68,8 @@ case $key in
     FILE2="$2"
     shift # past argument
     ;;
-     -r|--ready)
-    READY="YES"
+    -v|--verbosity)
+    VERBOSITY="YES"
     ;;
     *)
             # unknown option
